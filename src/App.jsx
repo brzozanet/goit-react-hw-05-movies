32dc9035dad0ef21a2d5990d5399a76df9670b7e
Paweł Brzozowski <pawel@brzoza.net>
2024-01-22 21:42:42 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas expedita
+        quisquam veritatis incidunt consectetur! Voluptas voluptates velit
+        voluptatum commodi corrupti.
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aliquam
+        sequi neque libero ullam dicta voluptatibus in similique veritatis
+        corporis quidem, ratione doloremque mollitia expedita animi voluptate
+        dolores illo quas eligendi non totam perferendis! Necessitatibus
+        recusandae culpa libero nulla iusto velit architecto distinctio
+        similique exercitationem tempora incidunt itaque dolorum omnis iste sit
+        fugiat error nobis ullam voluptatum alias, esse optio suscipit quaerat
+        ut? Quibusdam commodi labore magnam provident sunt pariatur enim quam
+        rem iure. Similique molestiae explicabo alias quos voluptas voluptatem
+        dicta eaque totam minus maiores magnam nulla iure assumenda consequatur
+        vel natus corrupti excepturi laboriosam, non debitis, voluptatibus
+        provident!
+      </p>
+    </>
+  );
+}
