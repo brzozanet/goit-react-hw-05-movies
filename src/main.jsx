fdf3c34a713905f5bf46462f90b69240938df058
Paweł Brzozowski <pawel@brzoza.net>
2024-02-05 18:46:35 +0100

    Fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -56,5 +56,5 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={router} basename="/" />
 );
