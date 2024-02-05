b57140982331c5c25888de7862e51db807a9e2f6
Paweł Brzozowski <pawel@brzoza.net>
2024-02-05 20:42:45 +0100

    Remove basename prop

--- src/main.jsx
+++ src/main.jsx
@@ -56,5 +56,5 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} basename="/" />
+  <RouterProvider router={router} />
 );
